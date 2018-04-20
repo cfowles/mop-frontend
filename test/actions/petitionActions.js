36eb8ad2d78e7736c0d3a804e1988f91715b7214
Justin Parker <justin@jpark.tech>
2018-04-20 09:14:30 -0400

    Test for submitting a signature

---
+++ test/actions/petitionActions.js
@@ -0,0 +1,65 @@
+import { expect } from 'chai'
+import sinon from 'sinon'
+import fetchMock from 'fetch-mock'
+import Config from '../../src/config'
+
+import { signPetition } from '../../src/actions/petitionActions'
+
+const SQS_RESPONSE = {
+  SendMessageResponse: {
+    ResponseMetadata: { RequestId: 'abc' },
+    SendMessageResult: {
+      MD5OfMessageAttributes: 'def',
+      MD5OfMessageBody: 'ghi',
+      MessageId: 'jkl3',
+      SequenceNumber: null
+    }
+  }
+}
+
+const SIGNATURE = {
+  signature: true,
+  anything: true
+}
+
+const PETITION = {
+  petition: true,
+  anything: true
+}
+
+describe('petitionActions', () => {
+  let prevSign
+  let prevWritable
+  before(() => {
+    prevSign = Config.API_SIGN_PETITION
+    prevWritable = Config.API_WRITABLE
+    Config.API_SIGN_PETITION = 'https://petitions.example.com/api-v1-signatures'
+    Config.API_WRITABLE = true
+    fetchMock.post('https://petitions.example.com/api-v1-signatures', SQS_RESPONSE)
+  })
+
+  it('calls sign petition endpoint', done => {
+    const dispatch = sinon.spy(() => {
+      if (dispatch.callCount === 2) {
+        expect(fetchMock.lastUrl()).to.equal('https://petitions.example.com/api-v1-signatures')
+        const opts = fetchMock.lastOptions()
+        expect(opts.method).to.equal('POST')
+        expect(opts.body).to.equal(JSON.stringify(SIGNATURE))
+
+        expect(dispatch.firstCall.args[0].type).to.equal(
+          'PETITION_SIGNATURE_SUBMIT'
+        )
+        expect(dispatch.secondCall.args[0].type).to.equal(
+          'PETITION_SIGNATURE_SUCCESS'
+        )
+        done()
+      }
+    })
+    signPetition(SIGNATURE, PETITION)(dispatch)
+  })
+  after(() => {
+    fetchMock.restore()
+    Config.API_SIGN_PETITION = prevSign
+    Config.API_WRITABLE = prevWritable
+  })
+})
