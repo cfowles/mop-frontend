64c000fa3bac99c2dff507d10300264657c97b2b
Justin Parker <justin@jpark.tech>
2018-04-23 19:08:54 -0400

    Nav: Change outside links to href

--- src/giraffe-ui/nav/navlink.js
+++ src/giraffe-ui/nav/navlink.js
@@ -2,13 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router'
 
-export const NavLink = ({ to, children }) => (
+export const NavLink = ({ to, href, children }) => (
   <li>
-    <Link to={to}>{children}</Link>
+    {to && <Link to={to}>{children}</Link>}
+    {!to && href && <a href={href}>{children}</a>}
   </li>
 )
 
 NavLink.propTypes = {
   to: PropTypes.string,
+  href: PropTypes.string,
   children: PropTypes.node
 }
