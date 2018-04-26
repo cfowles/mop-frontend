3ddb2b2d2b4ed9b94b62970d9b8d4cf43626b72a
Justin Parker <justin@jpark.tech>
2018-04-26 11:02:20 -0400

    Broken: Update enzyme for tests [skip ci]

--- test-setup.js
+++ test-setup.js
@@ -1,4 +1,9 @@
+import enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
 import requireHacker from 'require-hacker'
+
+enzyme.configure({ adapter: new Adapter() })
 
 // Replace svgs with empty react components
 requireHacker.hook('svg', () => 'module.exports = () => null')
