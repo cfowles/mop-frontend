a3db6b45ca48e76f219b03fac15c638f942e10f5
Justin Parker <justin@jpark.tech>
2018-04-26 10:52:54 -0400

    Close the menu when navigating internally from the nav

--- src/giraffe-ui/nav/navlink.js
+++ src/giraffe-ui/nav/navlink.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router'
+import { MoNavContext } from './header'
 
-export const NavLink = ({ to, href, children }) => (
-  <li>
-    {to && <Link to={to}>{children}</Link>}
-    {!to && href && <a href={href}>{children}</a>}
-  </li>
-)
+export const NavLink = ({ to, href, children }) => {
+  if (href) {
+    // render an external link
+    return (
+      <li>
+        <a href={href}>{children}</a>
+      </li>
+    )
+  }
+
+  // render an internal link that also closes the mobile menu
+  return (
+    <li>
+      <MoNavContext.Consumer>
+        {({ close }) => (
+          <Link to={to} onClick={close}>
+            {children}
+          </Link>
+        )}
+      </MoNavContext.Consumer>
+    </li>
+  )
+}
 
 NavLink.propTypes = {
   to: PropTypes.string,
