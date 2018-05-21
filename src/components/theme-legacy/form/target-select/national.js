6bc58c34e1c8ab0aba702c4541134cb771167d3e
Justin Parker <justin@jpark.tech>
2018-05-21 09:12:40 -0400

    Refactor national target

--- src/components/theme-legacy/form/target-select/national.js
+++ src/components/theme-legacy/form/target-select/national.js
@@ -3,10 +3,31 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import LegislatorAutocomplete from './legislator-autocomplete'
 
+const isDefault = target =>
+  ['president', 'house', 'senate'].indexOf(target.target_type) !== -1
+
 const NationalTargetSelect = ({ selected, onSelect, remove, items }) => (
   <div>
     <div id='national_group_checkboxes'>
-      {selected.map(selectedItem => (
+      {items.filter(isDefault).map(defaultItem => {
+        const isChecked = selected.filter(t => t.value === defaultItem.value)
+          .length
+        return (
+          <div key={defaultItem.label} className='checkbox wrapper'>
+            <label>
+              <input
+                type='checkbox'
+                onChange={() =>
+                  (isChecked ? remove(defaultItem) : onSelect(defaultItem))
+                }
+                checked={isChecked}
+              />{' '}
+              {defaultItem.label}
+            </label>
+          </div>
+        )
+      })}
+      {selected.filter(t => !isDefault(t)).map(selectedItem => (
         <div key={selectedItem.label} className='checkbox wrapper'>
           <label>
             <input
@@ -23,7 +44,7 @@ const NationalTargetSelect = ({ selected, onSelect, remove, items }) => (
       <LegislatorAutocomplete
         group='national'
         onChange={onSelect}
-        items={items}
+        items={items.filter(t => !isDefault(t))}
       />
     </div>
   </div>
