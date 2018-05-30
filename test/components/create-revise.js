cb5eff5ea9d5ca2f683a0861a59107e460a9bfc0
Justin Parker <justin@jpark.tech>
2018-05-30 13:44:49 -0400

    Tests for create-revise

---
+++ test/components/create-revise.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { expect } from 'chai'
+
+import { Provider } from 'react-redux'
+import { createMockStore } from 'redux-test-utils'
+
+import { mount } from 'enzyme'
+
+import CreateRevise from '../../src/containers/create-revise'
+import { CreatePetition } from '../../src/containers/create-petition'
+
+
+describe('<CreateRevise />', () => {
+  const petition = {
+    title: 'testtitle',
+    target: [
+      {
+        label: 'Governor of Florida',
+        target_type: 'governor',
+        target_id: 'FL'
+      }],
+    summary: 'testsummary',
+    description: 'testdescription'
+  }
+
+  const store = createMockStore({
+    petitionCreateStore: petition
+  })
+
+  const revise = mount(
+    <Provider store={store}>
+      <CreateRevise />
+    </Provider>
+  )
+
+  it('loads the create page', () => {
+    expect(revise.text()).to.contain('Start your petition')
+  })
+
+  it('displays title', () => {
+    expect(revise.find('input[name="title"]').prop('value')).to.equal('testtitle')
+  })
+
+  it('displays target', () => {
+    expect(revise.text()).to.contain('Governor of Florida')
+  })
+
+  it('displays summary', () => {
+    expect(revise.find('textarea[name="summary"]').text()).to.equal('testsummary')
+  })
+
+  it('displays description', () => {
+    expect(revise.text()).to.contain('testdescription')
+  })
+
+  it('passes the stored petition to <CreatePetition />', () => {
+    const createPetition = revise.find(CreatePetition)
+    expect(createPetition.length).to.equal(1)
+    expect(createPetition.prop('initialPetition')).to.equal(petition)
+  })
+
+  it('redirects when there is no petition', () => {
+    mount(
+      <Provider store={createMockStore({ petitionCreateStore: {} })}>
+        <CreateRevise />
+      </Provider>
+    )
+    expect(document.location.toString()).to.contain('/create_start.html')
+  })
+})
