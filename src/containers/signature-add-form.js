475d543fabcaf2a7839fd78e094da57dc1fc5162
Justin Parker <justin@jpark.tech>
2018-06-05 12:27:47 -0400

    Show signing fields if address is required

--- src/containers/signature-add-form.js
+++ src/containers/signature-add-form.js
@@ -204,8 +204,9 @@ class SignatureAddForm extends React.Component {
 
     if (this.formIsValid()) {
       const osdiSignature = this.getOsdiSignature()
-      dispatch(signAction(osdiSignature, petition, { redirectOnSuccess: true }))
+      return dispatch(signAction(osdiSignature, petition, { redirectOnSuccess: true }))
     }
+    this.setState({ hideUntilInteract: false }) // show fields so we can show validation error
     return false
   }
 
@@ -248,7 +249,8 @@ class SignatureAddForm extends React.Component {
         setRef={setRef}
         innerRef={innerRef}
         id={id}
-        hideUntilInteract={this.state.hideUntilInteract}
+        // Don't hide if we need to show address fields
+        hideUntilInteract={showAddressFields ? false : this.state.hideUntilInteract}
       />
     )
   }
