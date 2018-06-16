a1dd2046261401c1264f92c3c91e8efbba5c6667
Justin Parker <justin@jpark.tech>
2018-06-16 08:27:59 -0500

    Fix jsdom and react for tests

--- test-setup.js
+++ test-setup.js
@@ -6,3 +6,6 @@ enzyme.configure({ adapter: new Adapter() })
 
 // Replace svgs with empty react components
 requireHacker.hook('svg', () => 'module.exports = () => null')
+
+// Pass pretendToBeVisual so react has access to requestAnimationFrame
+require('jsdom-global')(undefined, { pretendToBeVisual: true })
