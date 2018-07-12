9c3777f46a57c1714a59907d6175333bcaa3a279
Justin Parker <justin@jpark.tech>
2018-07-12 17:29:39 -0400

    Remove body css from create.scss and instead depend on wrapper

--- src/components/theme-giraffe/wrapper.js
+++ src/components/theme-giraffe/wrapper.js
@@ -1,13 +1,32 @@
 import React from 'react'
+import cx from 'classnames'
 import PropTypes from 'prop-types'
 import Nav from '../../containers/nav'
 import Footer from 'Theme/footer'
 
-const Wrapper = ({ children, organization, minimalNav, entity }) => (
-  <div id='giraffe-wrapper' className='giraffe petition'>
-    <Nav organization={organization} minimal={minimalNav} entity={entity} />
-    <main id='content'>{children}</main>
-    <Footer entity={entity} />
+const Wrapper = ({
+  children,
+  organization,
+  minimalNav,
+  hideNav,
+  hideFooter,
+  offWhiteBg,
+  entity
+}) => (
+  <div className='giraffe'>
+    <div
+      id='giraffe-wrapper'
+      className={cx('giraffe', 'petition', {
+        'bg-off-white': offWhiteBg,
+        'pt-0': hideNav
+      })}
+    >
+      {!hideNav && (
+        <Nav organization={organization} minimal={minimalNav} entity={entity} />
+      )}
+      <main id='content'>{children}</main>
+      {!hideFooter && <Footer entity={entity} />}
+    </div>
   </div>
 )
 
@@ -15,6 +34,9 @@ Wrapper.propTypes = {
   children: PropTypes.node.isRequired,
   organization: PropTypes.string,
   minimalNav: PropTypes.bool,
+  hideNav: PropTypes.bool,
+  hideFooter: PropTypes.bool,
+  offWhiteBg: PropTypes.bool,
   entity: PropTypes.string
 }
 
